50486385b1a4a2f3fb2c3bc83a944cc45a81a881
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-16 15:29:15 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function zeigeStatus () {
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, "" + car4.format(car4.motorAget(), 3, car4.eAlign.right) + car4.format(car4.servo_get(), 4, car4.eAlign.right) + "" + car4.format(car4.encoder_get(car4.eEncoderEinheit.Impulse), 5, car4.eAlign.right))
+}
 radio.onReceivedBuffer(function (Datenpaket) {
     car4.onReceivedBuffer(Datenpaket)
     if (!(car4.isConnected())) {
@@ -12,8 +15,6 @@ radio.onReceivedBuffer(function (Datenpaket) {
         } else {
             car4.motorA255(128)
         }
-    } else {
-    	
     }
 })
 control.onEvent(car4.encoder_EventSource(), EventBusValue.MICROBIT_EVT_ANY, function () {
