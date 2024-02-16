b4b749d796503ff78e47649b40946868f69bae3d
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-16 20:35:32 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,11 +17,36 @@ radio.onReceivedBuffer(function (Datenpaket) {
         } else {
             car4.motorA255(128)
         }
+        zeigeStatus()
+        Helligkeit()
     }
 })
 control.onEvent(car4.encoder_EventSource(), EventBusValue.MICROBIT_EVT_ANY, function () {
     basic.setLedColor(0x007fff)
 })
+function Helligkeit () {
+    if (car4.licht_get() && car4.helligkeit_vergleich(car4.eVergleich.gt, 300)) {
+        car4.licht(false)
+    } else if (!(car4.licht_get()) && car4.helligkeit_vergleich(car4.eVergleich.lt, 200)) {
+        car4.licht(true)
+    }
+}
 car4.beimStart(240, 90)
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text(car4.logText()))
+loops.everyInterval(1000, function () {
+    if (car4.lastConnected(car4.car4_ePause(car4.ePause.p60))) {
+        car4.relay(false)
+    } else if (car4.isConnected() && car4.lastConnected(car4.car4_ePause(car4.ePause.p1))) {
+        car4.comment("zwischen 1 Sekunde und 1 Minute ohne Bluetooth: Standby und blinken")
+        car4.comment("einmalig nach neu disconnected")
+        car4.setConnected(false)
+        car4.motorON(false)
+    } else if (false) {
+        car4.comment("dauerhaft wenn disconnected")
+        car4.licht(true, true)
+        zeigeStatus()
+    } else {
+        car4.comment("Bluetooth ist verbunden: 'wenn Zahl empfangen' ist aktiv")
+    }
+})
