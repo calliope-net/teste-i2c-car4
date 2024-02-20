d305f912a5d5cc4e31583244719f01e0d2d2d377
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-20 13:02:43 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showNumber(car4.receivedBuffer_getUint8(car4.eBuffer.b0_Motor))
+})
 function zeigeStatus () {
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text(car4.statuszeile1(car4.eStatuszeile.a)))
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 7, lcd16x2rgb.lcd16x2_text(car4.statuszeile1(car4.eStatuszeile.b)))
@@ -14,9 +17,9 @@ radio.onReceivedBuffer(function (Datenpaket) {
             car4.setConnected(true)
             car4.licht(false)
         } else if (car4.isConnected()) {
-            if (car4.receivedBuffer_Contains(car4.eBuffer.b1_Servo)) {
+            if (car4.receivedBuffer_Contains()) {
                 car4.servo(car4.receivedBuffer_getUint8(car4.eBuffer.b1_Servo))
-                car4.motorON(car4.receivedBuffer_getBit(car4.eBit.x80_MotorPower))
+                car4.motorON(false)
                 car4.motorA255(car4.receivedBuffer_getUint8(car4.eBuffer.b0_Motor))
             } else {
                 car4.motorA255(128)
@@ -29,9 +32,6 @@ radio.onReceivedBuffer(function (Datenpaket) {
 control.onEvent(car4.encoder_EventSource(), EventBusValue.MICROBIT_EVT_ANY, function () {
 	
 })
-car4.beimStart(240, 97)
-lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
-lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text(car4.statuszeile1(car4.eStatuszeile.start)))
 loops.everyInterval(1000, function () {
     if (car4.car4ready()) {
         if (car4.lastConnected(car4.car4_ePause(car4.ePause.p60))) {
