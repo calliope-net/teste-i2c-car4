065b5bd6fffd66f98e54ca873132c0d22b13448c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-24 20:15:08 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+car4.onReceivedData(function (receivedBuffer) {
+    if (car4.receivedBuffer_Contains()) {
+        car4.servo(car4.receivedBuffer_getUint8(car4.eBufferOffset.b1_Servo))
+        car4.motorON(car4.receivedBuffer_getBit(car4.eBufferBit.x80_MotorPower))
+        car4.motorA255(car4.receivedBuffer_getUint8(car4.eBufferOffset.b0_Motor))
+        car4.buzzer(car4.receivedBuffer_getBit(car4.eBufferBit.x40_Hupe))
+    } else {
+        car4.motorA255(128)
+    }
+    zeigeStatus()
+    car4.licht_sensor(200, 300)
+})
 function zeigeStatus () {
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text(car4.statuszeile1(car4.eStatuszeile.a)))
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 7, z)
@@ -11,32 +23,12 @@ pins.onPulsed(DigitalPin.C16, PulseValue.Low, function () {
     car4.comment("Zeit wie lange es low ↓↑ war in µs")
     if (pins.pulseDuration() > 100000) {
         car4.motorON(true)
-        car4.fahreSchritt(car4.programmSchritt(-81, 41, 30))
+        car4.fahreSchritt(car4.programmSchritt(-60, 90, 30))
         z += 1
     }
 })
 car4.onEncoderStop(function (v) {
     car4.motorA255(128)
-})
-car4.onReceivedData2(function () {
-    if (car4.car4ready()) {
-        if (!(car4.isConnected())) {
-            car4.comment("einmalig nach neu connected")
-            car4.setConnected(true)
-            car4.licht(false)
-        } else if (car4.isConnected()) {
-            if (car4.receivedBuffer_Contains()) {
-                car4.servo(car4.receivedBuffer_getUint8(car4.eBufferOffset.b1_Servo))
-                car4.motorON(car4.receivedBuffer_getBit(car4.eBufferBit.x80_MotorPower))
-                car4.motorA255(car4.receivedBuffer_getUint8(car4.eBufferOffset.b0_Motor))
-                car4.buzzer(car4.receivedBuffer_getBit(car4.eBufferBit.x40_Hupe))
-            } else {
-                car4.motorA255(128)
-            }
-            zeigeStatus()
-            car4.licht_sensor(200, 300)
-        }
-    }
 })
 let z = 0
 car4.beimStart(240, 97)
