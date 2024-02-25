910aa47dc4b6db6ee462ebbe1bddb1ed6bc55bda
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-25 19:08:23 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 car4.onReceivedData(function (receivedBuffer) {
     if (car4.receivedBuffer_getBit(car4.eBufferBit.fahrenJostick)) {
         if (car4.receivedBuffer_Contains()) {
-            car4.servo(car4.receivedBuffer_getUint8(car4.eBufferOffset.b1_Servo))
+            car4.servo_set(car4.receivedBuffer_getUint8(car4.eBufferOffset.b1_Servo))
             car4.motorON(car4.receivedBuffer_getBit(car4.eBufferBit.x80_MotorPower))
             car4.motorA255(car4.receivedBuffer_getUint8(car4.eBufferOffset.b0_Motor))
             car4.buzzer(car4.receivedBuffer_getBit(car4.eBufferBit.x40_Hupe))
@@ -18,7 +18,7 @@ car4.onReceivedData(function (receivedBuffer) {
 })
 function zeigeStatus () {
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text(car4.statuszeile1(car4.eStatuszeile.a)))
-    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 7, z)
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 7, car4.statuszeile1(car4.eStatuszeile.b))
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 8, 15, car4.statuszeile1(car4.eStatuszeile.c))
 }
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
@@ -38,17 +38,11 @@ pins.onPulsed(DigitalPin.C16, PulseValue.Low, function () {
         z += 1
     }
 })
-car4.onEncoderStop(function (v) {
-    car4.motorA255(128)
-})
 let z = 0
 car4.beimStart(240, 97)
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text(car4.statuszeile1(car4.eStatuszeile.start)))
 loops.everyInterval(1000, function () {
-	
-})
-loops.everyInterval(500, function () {
     if (car4.bluetooth_timeout()) {
         zeigeStatus()
     }
