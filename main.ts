2c2c86919ea4224a08077d75d84828daf3384354
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-26 12:26:52 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -34,7 +34,8 @@ pins.onPulsed(DigitalPin.C16, PulseValue.Low, function () {
     car4.comment("Zeit wie lange es low ↓↑ war in µs")
     if (pins.pulseDuration() > 100000) {
         car4.motorON(true)
-        car4.fahreSchritt(car4.programmSchritt(-60, 90, 30))
+        car4.fahreSchritt(car4.programmSchritt(50, 150, 250))
+        car4.fahreSchritt(car4.programmSchritt(-20, 30, 250))
         z += 1
     }
 })
